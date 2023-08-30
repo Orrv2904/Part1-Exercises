640570def490afd5a1f86bf3d7852ca98721cbc3
Orrv2904 <Omarrv2904@gmail.com>
2023-08-30 14:38:23 -0600

    refactoring the Content component

--- Part1/courseinfo/src/index.js
+++ Part1/courseinfo/src/index.js
@@ -1,25 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Content from './content';
 
 const Header = (props) => {
   return (
     <h1>{props.course}</h1>
-  );
-}
-
-const Part = (props) => {
-  return (
-    <p>{props.part} {props.exercises}</p>
-  );
-}
-
-const Content = (props) => {
-  return (
-    <div>
-      <Part part={props.part1} exercises={props.exercises1} />
-      <Part part={props.part2} exercises={props.exercises2} />
-      <Part part={props.part3} exercises={props.exercises3} />
-    </div>
   );
 }
 
